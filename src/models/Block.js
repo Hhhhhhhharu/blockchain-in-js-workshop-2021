9cde79e58292a50d21d31ac11c4355926f7d140e
Hhhhhhhharu <1262389831@qq.com>
2023-05-18 23:45:08 +0800

    update

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,18 +1,35 @@
+import pkg from 'crypto-js';
+const { SHA256 } = pkg;
+
+
+export const DIFFICULTY = 3
 class Block {
   // 1. 完成构造函数及其参数
-  /* 构造函数需要包含
-   所属区块链
-   父块哈希
-   高度
-   哈希
-  */
-  constructor(blockchain,previousHash,height,hash) {
+
+  
+  constructor(blockchain,previousHash,height,hash,nonce) {
     this.blockchain=blockchain;
     this.previousHash=previousHash;
     this.height=height
     this.hash=hash
-
+    this.nonce=nonce
   }
+  //检查当前区块的哈希是否符合难度需求
+  //我们要检查当前哈希值前缀中0的数量是否等于difficulty，或者说是大于等于，就认为满足要求
+  isValid() {
+    const hash=this.calculateHash()
+    return hash.substring(0,DIFFICULTY) == '0'.repeat(DIFFICULTY)
+  }
+  //计算当前区块的哈希值
+  //我把 父块哈希加高度加随机数 的哈希值当前区块的哈希值
+   calculateHash(){
+    return SHA256(
+      this.previousHash+this.height+this.nonce
+    ).toString()
+   }
+   //设置随机数
+  setNonce(nonce) {
+    this.nonce=nonce}
 }
 
 export default Block
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,14 @@
 import sha256 from 'crypto-js/sha256.js'
 
 export const validateHash = () => {}
-
+//定义一个参数接收block
 export const calcNonce = (block) => {
   console.log(`calc nonce of block ${block.height} `)
+  //获取时间戳
   const start = new Date().getTime()
   let calcTimes = 0
+
+  //先看block是否符合难度要求，不符合就calcTime加一
   while (!block.isValid()) {
     block.setNonce(sha256(new Date().getTime().toString()).toString())
     calcTimes++
